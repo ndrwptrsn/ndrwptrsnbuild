a435ba9c08d1c609ec438ab073aa4fbd275d6359
ndrwptrsn <peterson8000@yahoo.com>
2021-03-09 09:18:52 -0800

    correct scrolling behavior

--- src/scripts/starfield.js
+++ src/scripts/starfield.js
@@ -140,8 +140,8 @@ var Starfield = (function() {
     // camera.position.x += ( mouseX - camera.position.x ) * 0.005;
     // camera.position.y += ( - mouseY - camera.position.y ) * 0.005;
 
-    starfield.rotation.x -= randomNumbers[0]/30000;
-    starfield.rotation.y += randomNumbers[1]/30000;
+    starfield.rotation.x -= randomNumbers[0]/5000;
+    starfield.rotation.y += randomNumbers[1]/5000;
 
     renderer.render( scene, camera );
   }

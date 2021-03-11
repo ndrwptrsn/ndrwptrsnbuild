4a371e252ca36c6d1905e371cd77ef1f7f2182c8
ndrwptrsn <peterson8000@yahoo.com>
2021-03-10 22:47:02 -0800

    fix orbs popping in and out at edges

--- src/scripts/starfield.js
+++ src/scripts/starfield.js
@@ -18,7 +18,7 @@ var Starfield = (function() {
     // colors
     document.body.style.background = colors[3];
     document.getElementById("main").style.background = 'linear-gradient(to bottom, ' + colors[3] + ',' + colors[1] + ')';
-
+    //
     container = ( typeof element == 'string') ? document.getElementById(element) : element;
     measure();
     setupCamera();
@@ -28,7 +28,7 @@ var Starfield = (function() {
 
     // Event listeners
     $(document).mousemove(onMouseMove);
-    $(window).resize(onResize);
+    // $(window).resize(onResize);
   }
 
   function measure() {
@@ -40,7 +40,7 @@ var Starfield = (function() {
 
   function setupCamera() {
      if ( !camera ) {
-      camera = new THREE.Camera( 75, windowWidth / windowHeight, 1, 3000 );
+      camera = new THREE.PerspectiveCamera( 75, windowWidth / windowHeight, 1, 3000 );
       camera.position.z = 1000;
      }
   }
@@ -48,17 +48,20 @@ var Starfield = (function() {
   function setupScene() {
     if ( !scene ) {
       starfield = new THREE.Object3D();
+
       addCelestialObjectsTo(starfield, 570, createStar);
       addCelestialObjectsTo(starfield, 300, createRedDwarf);
 
+      starfield.frustumCulled = false;
+
       scene = new THREE.Scene();
-      scene.addObject( starfield );
+      scene.add( starfield );
     }
   }
 
   function createRenderer() {
     if ( !renderer ) {
-      renderer = new THREE.CanvasRenderer();
+      renderer = new THREE.WebGLRenderer({ alpha: true });
       renderer.setSize(windowWidth, windowHeight);
       container.appendChild(renderer.domElement);
     }
@@ -68,25 +71,30 @@ var Starfield = (function() {
     return Math.random();
   }
 
-  function createStar(ctx) {
-    var gradient = ctx.createRadialGradient(8, 8, 0, 8, 8, 8);
-    gradient.addColorStop(0,    'rgba(255,255,255,.8)' );
-    gradient.addColorStop(randomNumbers[0],  colors[1] );
-    gradient.addColorStop(randomNumbers[1],  colors[2] );
-    gradient.addColorStop(randomNumbers[2],  colors[3] );
-    gradient.addColorStop(1,    'rgba(0,0,0,0)' );
-
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, 16, 16);
-  }
+  // function createStar() {
+  //   var gradient = ctx.createRadialGradient(8, 8, 0, 8, 8, 8);
+  //   gradient.addColorStop(0,    'rgba(255,255,255,.8)' );
+  //   gradient.addColorStop(randomNumbers[0],  colors[1] );
+  //   gradient.addColorStop(randomNumbers[1],  colors[2] );
+  //   gradient.addColorStop(randomNumbers[2],  colors[3] );
+  //   gradient.addColorStop(1,    'rgba(0,0,0,0)' );
+  //
+  //   ctx.fillStyle = gradient;
+  //   ctx.fillRect(0, 0, 16, 16);
+  // }
 
   function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
   }
 
-  function createRedDwarf(ctx) {
-    var gradient = ctx.createRadialGradient(8, 8, 0, 8, 8, 8);
+  function createRedDwarf() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 1600;
+    canvas.height = 1600;
+
+    var ctx = canvas.getContext('2d');
+    var gradient = ctx.createRadialGradient(800, 800, 0, 800, 800, 800);
     gradient.addColorStop(0,    'rgba(255,255,255,.8)' );
     gradient.addColorStop(randomNumbers[2],  colors[4] );
     gradient.addColorStop(randomNumbers[0],  colors[5] );
@@ -94,21 +102,43 @@ var Starfield = (function() {
     gradient.addColorStop(1,    'rgba(0,0,0,0)' );
 
     ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, 16, 16);
+    ctx.fillRect(0, 0, 1600, 1600);
+    return canvas;
+  }
+
+  function createStar() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 1600;
+    canvas.height = 1600;
+
+    var ctx = canvas.getContext('2d');
+    var gradient = ctx.createRadialGradient(800, 800, 0, 800, 800, 800);
+    gradient.addColorStop(0,    'rgba(255,255,255,.8)' );
+    gradient.addColorStop(randomNumbers[0],  colors[1] );
+    gradient.addColorStop(randomNumbers[1],  colors[2] );
+    gradient.addColorStop(randomNumbers[2],  colors[3] );
+    gradient.addColorStop(1,    'rgba(0,0,0,0)' );
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, 1600, 1600);
+    return canvas;
   }
 
   function addCelestialObjectsTo(group, max, func) {
     var celObj;
-    var material = new THREE.ParticleCanvasMaterial({color: 0xffffff, program: func});
-
-    for(var i=0; i < max; ++i) {
-      celObj = new THREE.Particle(material);
+    let canv = func();
+    var amap = new THREE.Texture(canv);
+    amap.needsUpdate = true;
+    var material = new THREE.SpriteMaterial({ map: amap, color: 0xffffff });
+    for (var i = 0; i < max; ++i) {
+      celObj = new THREE.Sprite(material);
       celObj.position.x = Math.random() * 2000 - 1000;
       celObj.position.y = Math.random() * 2000 - 1000;
       celObj.position.z = Math.random() * 2000 - 1000;
-      celObj.scale.x = celObj.scale.y = Math.random() * (Math.floor(Math.random() * 40) + 1);
-
-      group.addChild(celObj);
+      let scale = Math.random() * (Math.floor(Math.random() * 500) + 1);
+      celObj.scale.set( scale,scale, scale);
+      celObj.frustumCulled = false;
+      group.add(celObj);
     }
   }
 
@@ -121,25 +151,22 @@ var Starfield = (function() {
     mouseY = e.clientY - windowHalfHeight;
   }
 
-  function onResize() {
-    measure();
-
-    if ( camera )
-      camera.aspect = windowWidth / windowHeight;
-
-    if ( renderer )
-      renderer.setSize(windowWidth, windowHeight);
-  }
+  // function onResize() {
+  //   measure();
+  //
+  //   if ( camera )
+  //     camera.aspect = windowWidth / windowHeight;
+  //
+  //   if ( renderer )
+  //     renderer.setSize(windowWidth, windowHeight);
+  // }
 
   window.addEventListener( 'resize', onWindowResize, false );
 
   function onWindowResize(){
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
   }
 
   // ---------------------------------------------------------------------------
@@ -151,7 +178,7 @@ var Starfield = (function() {
 
   function render() {
     camera.position.x += ( mouseX - camera.position.x ) * randomNumbers[1]/80;
-    camera.position.y += ( - mouseY - camera.position.y ) * randomNumbers[2]/80;
+    camera.position.y += ( mouseY - camera.position.y ) * randomNumbers[0]/80;
 
     starfield.rotation.x -= randomNumbers[0]/8000;
     starfield.rotation.y += randomNumbers[1]/8000;
